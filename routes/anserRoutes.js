ed40d1e405f752d5ff6266ee9f850d0ff2b57681
nuredin kemal <131611344+nura92@users.noreply.github.com>
2023-10-18 05:55:23 -0700

    Update anserRoutes.js
    answerout change group 2

--- routes/anserRoutes.js
+++ routes/anserRoutes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
 
+const { post_answer } = require("../controllers/answerController");
+
+router.post("/postanswer/:questions_id", post_answer);
+
+
+module.exports = router;
