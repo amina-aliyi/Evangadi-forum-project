abddbc3c90c4ac95c2a0ce6936ac43940356ab9f
zemichaell <71129973+zemichaell@users.noreply.github.com>
2023-10-18 10:40:49 -0700

    Update answerController.js
    getQuestionById

--- controllers/answerController.js
+++ controllers/answerController.js
@@ -23,6 +23,29 @@ const post_answer = async (req, res) => {
       .json({ msg: "somethin went to wrong try again later" });
   }
 };
+const getAnswerById = async (req, res) => {
+  const answerId = req.params.answerid;
+  //    const questionId = req.params.questionid;
 
+  try {
+    const [answer] = await dbConnection.query(
+      "SELECT questionid, userid, answer FROM answers WHERE answerid = ?",
+      [answerId]
+    );
 
-module.exports = { post_answer };
+    if (answer.length === 0) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: "Answer not found" });
+    }
+
+    return res.status(StatusCodes.OK).json({ answer });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+};
+
+module.exports = { post_answer getAnswerById };
